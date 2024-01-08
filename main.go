d299d0fde64947fc8a997b1d2a6c6caf62ef2da1
Jeff Lin <jeff.lin@riversoft.com.tw>
2024-01-08 14:49:01 +0800

    feat: 🎸 add example

--- main.go
+++ main.go
@@ -1,5 +1,19 @@
 package main
 
-func main() {
+import (
+	"fmt"
+)
 
+func printHello() {
+	fmt.Println("Hello")
+}
+
+func main() {
+	fmt.Println("start")
+
+	go printHello()
+
+	// answer
+	// time.Sleep(10 * time.Millisecond)
+	fmt.Println("end")
 }
