357a6a0acca1ed00dd46d9c340d0c7c65d65e298
Jeff Lin <jeff.lin@riversoft.com.tw>
2024-01-10 18:00:42 +0800

    feat: 🎸 update

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # golang_learning_concurrent
 
-在這個範例中將會發現為何沒有印出 Hello，原因是因爲 main 已經執行完畢，且不會等待其他 goroutine，但是我們知道 goroutine 被阻塞時，就會把控制全轉交給其他 goroutine，所以我們可以用 time.Sleep() 來阻塞 main 這個 goroutine
+在這範例中會學到使用 for loop 或是 for range 對 channel 進行讀取或是寫入的動作
 
 # reference
 https://pjchender.dev/golang/goroutine-channels-concurrency/
--- main.go
+++ main.go
@@ -4,16 +4,81 @@ import (
 	"fmt"
 )
 
-func printHello() {
-	fmt.Println("Hello")
+// // 範例一
+// func forloop(c chan int) {
+// 	// 把 0 ~ 9 寫入 channel 後便把 channel 關閉
+// 	for i := 0; i < 9; i++ {
+// 		c <- i
+// 	}
+// 	close(c)
+// }
+
+// // 範例二
+// func forrange(c chan int) {
+// 	// 把 0 ~ 9 寫入 channel 後便把 channel 關閉
+// 	for i := 0; i <= 9; i++ {
+// 		c <- i
+// 	}
+
+// 	// 若沒有 close 會 deadlock (for range)
+// 	close(c)
+// }
+
+// 範例三
+func forrange2(n int, c chan int) {
+	x, y := 0, 1
+	for i := 0; i < n; i++ {
+		c <- x
+		x, y = y, x+y
+	}
+
+	close(c)
 }
 
 func main() {
 	fmt.Println("start")
 
-	go printHello()
+	// // 範例一
+	// // for loop 搭配 close:需要手動 break 迴圈
+	// // 單純透過 for 迴圈需要自行判斷 channel 是否已經 close，如果是的話需要自行使用 break 把 for loop 終止
+	// c := make(chan int)
 
-	// answer
-	// time.Sleep(10 * time.Millisecond)
+	// go forloop(c)
+
+	// // 監聽 channel 的值：週期性的 block/unblock main goroutine 直到 squares goroutine close
+	// for {
+	// 	val, ok := <-c
+	// 	if !ok {
+	// 		fmt.Println(val, ok, "<- loop broke case channel closed")
+	// 		break
+	// 	} else {
+	// 		fmt.Println(val, ok)
+	// 	}
+	// }
+
+	// // 範例二
+	// // for range 搭配 close:會自動關閉迴圈
+	// // 在 go 中有 for range loop ，只要該 channel 被關閉後，loop 則會自動終止
+	// // 需特別留意，如果是在 main goroutine 中使用 for val := range channel {}，最後 channel 沒有被 close 的話程式會 deadlock。但如果是在其他的 goroutine 中使用，即使沒有 close 也不會 deadlock，但為了不必要的 bug 產生，一般都還是將其關閉
+
+	// c := make(chan int)
+
+	// go forrange(c)
+
+	// // 使用 for range 的寫法，一旦 channel close，loop 會自動 break
+	// for v := range c {
+	// 	fmt.Println(v)
+	// }
+
+	// 範例三
+	// for range 其他範例
+	c := make(chan int, 10)
+
+	go forrange2(cap(c), c)
+
+	for v := range c {
+		fmt.Println(v)
+	}
+
 	fmt.Println("end")
 }
